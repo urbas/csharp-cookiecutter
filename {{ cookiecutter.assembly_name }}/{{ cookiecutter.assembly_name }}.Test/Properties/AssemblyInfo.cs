46a423942a4acaa962c1d22787aece63fda273cc
Matej Urbas <matej.urbas@gmail.com>
2017-05-07 12:26:28 +0100

    Added csproj and sln files.

---
+++ {{ cookiecutter.assembly_name }}/{{ cookiecutter.assembly_name }}.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("{{ cookiecutter.assembly_name }}.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("{{ cookiecutter.author }}")]
+[assembly: AssemblyProduct("{{ cookiecutter.assembly_name }}.Test")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("E8AF31FC-229A-4DCB-B524-FFF1AFD4C0DA")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ {{ cookiecutter.assembly_name }}/{{ cookiecutter.assembly_name }}/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("{{ cookiecutter.assembly_name }}")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("{{ cookiecutter.author }}")]
+[assembly: AssemblyProduct("{{ cookiecutter.assembly_name }}")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5FDD6E0D-C1B4-48E8-96D1-CBB11D0BC774")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
